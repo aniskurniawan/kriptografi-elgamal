ca5bd691d2f76edde3da6cfe736f7ab605143c22
Anis Kurniawan <anis1400018179@webmail.uad.ac.id>
2019-05-10 04:59:00 +0700

    Add files via upload

---
+++ kripto/elgamal_dekripsi.php
@@ -0,0 +1,41 @@
+﻿<?php include "../fungsi/fungsi_elgamal.php";
+include("../assets/header.php");?>
+<a href="../index.php"><p align="left">Kembali</p></a>
+<h2 align="center">ALGORTMA ELGAMAL</h2>
+<p align="center">Merupakan salah satu algoritma kriptografi asimetris yang memiliki metode penyandian kunci publik, dengan menggunakan algoritma blok chipper yang melakukan proses enkripsi pada blok-blok plainteks yang kemudian menghasilkan blok-blok chipertext, yang nantinya blok-blok chipertext tersebut akan didekripsi kembali dan hasilnya kemudian digabungkan menjadi plainteks semula.</p>
+<div class="tab">
+    <button class="tablinks" onclick="openCity(event, 'Enkripsi')">Enkripsi</button>
+    <button class="tablinks" onclick="openCity(event, 'Dekripsi')" id="defaultOpen">Dekripsi</button>
+</div>
+<div id="Enkripsi" class="tabcontent">
+    <h3 align="center">Proses Enkripsi</h3><hr>
+    <p><b>Pembangkitan kunci :</b></p> 
+    <form method="post" action="elgamal_enkripsi.php">
+    <input type="number" placeholder="p (Bilangan Prima > 255)" name="p" required>
+    <input type="number" placeholder="g (g < p)" name="g" required>
+    <p><b>Pesan :</b></p> 
+    <textarea min-height="100px" max-height="100px" min-width="100%" max-width="100%" rows="4" cols="50" placeholder="Plainteks" name="teks" id="teks" required required></textarea>
+    <button type="submit" class="submit" name="enkripsi" id="enkripsi" value="enkripsi">Enkripsi</button>
+    </form>
+</div>
+<div id="Dekripsi" class="tabcontent">
+    <h3 align="center">Proses Dekripsi</h3><hr>
+    <p><b>Kunci Privat (x, p) :</b></p> 
+    <form method="post" action="elgamal_dekripsi.php">
+    <input type="number" placeholder="x" name="x" required>
+    <input type="number" placeholder="p" name="p" required>
+    <p><b>Pesan :</b></p> 
+    <textarea min-height="100px" max-height="100px" min-width="100%" max-width="100%" rows="4" cols="50" placeholder="Chiperteks" name="teks" id="teks" required></textarea>
+    <button type="submit" class="submit" name="dekripsi" id="dekripsi" value="dekripsi">Dekripsi</button>
+    </form>
+    
+    <?php
+    if (isset($_POST['dekripsi'])){
+        echo '<p style="color:red;">'.$duration.' detik</p>
+        Kunci Privat (x, p) = (<b>'.$x.'</b>, <b>'.$p.'</b>)
+        <br/><br/>
+        <p><b>Hasil dekripsi pesan <span style="color:blue;">'.$teks.'</span> :</b></p> <textarea min-height="100px" max-height="100px" min-width="100%" max-width="100%" rows="4" cols="50" placeholder="Chiperteks" name="teks" id="teks" disabled>'.$hasildekripsi.' </textarea>';
+    }
+    ?>
+</div>
+<?php include("../assets/footer.php");?>
---
+++ kripto/elgamal_enkripsi.php
@@ -0,0 +1,46 @@
+<?php include "../fungsi/fungsi_elgamal.php";
+include("../assets/header.php");?>
+<a href="../index.php"><p align="left">Kembali</p></a>
+<h2 align="center">ALGORTMA ELGAMAL</h2>
+<p align="center">Merupakan salah satu algoritma kriptografi asimetris yang memiliki metode penyandian kunci publik, dengan menggunakan algoritma blok chipper yang melakukan proses enkripsi pada blok-blok plainteks yang kemudian menghasilkan blok-blok chipertext, yang nantinya blok-blok chipertext tersebut akan didekripsi kembali dan hasilnya kemudian digabungkan menjadi plainteks semula.</p>
+<div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'Enkripsi')" id="defaultOpen">Enkripsi</button>
+  <button class="tablinks" onclick="openCity(event, 'Dekripsi')">Dekripsi</button>
+</div>
+<div id="Enkripsi" class="tabcontent">
+    <h3 align="center">Proses Enkripsi</h3><hr>
+    <p><b>Pembangkitan kunci :</b></p> 
+    <form method="post" action="elgamal_enkripsi.php">
+    <input type="number" placeholder="p (Bilangan Prima > 255)" name="p" required>
+    <input type="number" placeholder="g (g < p)" name="g" required>
+    <p><b>Pesan :</b></p> 
+    <textarea min-height="100px" max-height="100px" min-width="100%" max-width="100%" rows="4" cols="50" placeholder="Plainteks" name="teks" id="teks" required required></textarea>
+    <button type="submit" class="submit" name="enkripsi" id="enkripsi" value="enkripsi">Enkripsi</button>
+    </form>
+    <?php if (isset($_POST['enkripsi'])){
+        echo '
+            <p style="color:red;">'.$duration.' detik</p>
+            <p><b>Kunci yang dibangkitkan dari <span style="color:blue;">
+            p = '.$p.'</span>, <span style="color:blue;"> g = '.$g.',</span> dan <span style="color:blue;">x = '.$x.'</span> :</b></p>
+            <p>
+            1. Nilai k secara acak (1 = k = p - 1) = <b>'.$k.'</b><br/>
+            2. Kunci Publik (y, g, p) = (<b>'.$y.'</b>, <b>'.$g.'</b>, <b>'.$p.'</b>)<br/>
+            3. Kunci Privat (x, p) = (<b>'.$x.'</b>, <b>'.$p.'</b>)
+
+            </p><br/>
+            <p><b>Hasil enkripsi pesan <span style="color:blue;">'.$teks.'</span> :</b></p> <textarea min-height="100px" max-height="100px" min-width="100%" max-width="100%" rows="4" cols="50" placeholder="Chiperteks" name="teks" id="teks" disabled>'.$hasilenkripsi.' </textarea>';
+    }
+    ?>
+</div>
+<div id="Dekripsi" class="tabcontent">
+    <h3 align="center">Proses Dekripsi</h3><hr>
+    <p><b>Kunci Privat (x, p) :</b></p> 
+    <form method="post" action="elgamal_dekripsi.php">
+    <input type="number" placeholder="x" name="x" required>
+    <input type="number" placeholder="p" name="p" required>
+    <p><b>Pesan :</b></p> 
+    <textarea min-height="100px" max-height="100px" min-width="100%" max-width="100%" rows="4" cols="50" placeholder="Chiperteks" name="teks" id="teks" required></textarea>
+    <button type="submit" class="submit" name="dekripsi" id="dekripsi" value="dekripsi">Dekripsi</button>
+    </form>
+</div>
+<?php include("../assets/footer.php");?>
